aa19923224f43db4523a544396b26ce1aeb48ea2
Dharun <dharun.bs2003@gmail.com>
2026-05-02 15:48:08 +0530

    Initial-Scaffold

---
+++ src/main/java/com/maintainsoft/MaintainsoftApplication.java
@@ -0,0 +1,13 @@
+package com.maintainsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaintainsoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaintainsoftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maintainsoft/MaintainsoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maintainsoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaintainsoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
