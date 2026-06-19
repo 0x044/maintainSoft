69dab21a00f60016f58f493956ded7077fb01377
Dharun <dharun.bs2003@gmail.com>
2026-06-19 14:13:30 +0530

    incremental entity hardening
    Added soft delete option for User entity
    Added version annotation (idk why lol, need to learn)

--- src/main/java/com/maintainsoft/controller/AuthController.java
+++ src/main/java/com/maintainsoft/controller/AuthController.java
@@ -5,9 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/api/v1/auth")
@@ -19,5 +17,4 @@ public class AuthController {
     ResponseEntity<String> hello(){
         return ResponseEntity.ok().body("hello!");
     }
-
 }
